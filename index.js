f794403246cda4ac5b4371025e5a67209a42ecff
Ahmed Hamoudi <hamoudi0427@gmail.com>
2023-02-20 22:42:33 -0500

    removed material ui dependancy

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const Person = require('./models/persons')
-const { NextWeek } = require('@material-ui/icons')
 
 const app = express()
 app.use(cors())
