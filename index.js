413119f9b99b2079148d0a7dd75cb5d6cb39d99b
Ahmed Hamoudi <hamoudi0427@gmail.com>
2023-03-12 12:19:07 -0400

    Added validation error for name

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ const handleError = (error, req, res, next) => {
     console.log(error.message)
 
     if (error.name == 'CastError') return res.status(400).send({error: 'malformatted id'})
+    else if (error.name == 'ValidationError') return res.status(400).json({error: error.message})
     next(error)
 }
 app.use(handleError)
